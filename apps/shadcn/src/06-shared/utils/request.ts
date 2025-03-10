d09a0aa5ae4f0ba77051adf1098397a48dcad760
kolirt <kolirt@pm.me>
2025-03-10 12:41:54 +0200

    update

--- apps/shadcn/src/06-shared/utils/request.ts
+++ apps/shadcn/src/06-shared/utils/request.ts
@@ -1,9 +1,6 @@
 import { type Method, type Options, request as requestMaster } from '@monorepo-starter/request'
 
-import { Layouts } from '@/02-pages'
-
 function request<T = any>(method: Method, url: string, data: Record<string, any> = {}, options: Options = {}) {
-  console.log(Layouts)
   return requestMaster<T>(method, url, data, {
     timeout: 5_000,
     ...options,
